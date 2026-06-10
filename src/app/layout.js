9cbd746539b9de508ac915bc9ccad06849f8c328
shohel-shihab <shihabcse21@gmail.com>
2026-06-10 16:44:16 +0600

    Install next.js hero ui and adding font for studynook app

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
   subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-poppins",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={poppins.className}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
